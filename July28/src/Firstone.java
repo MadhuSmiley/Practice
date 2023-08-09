8e7638cd28789e48f11b1d2b804e94ccd35f477e
madha <madha@192.168.2.102>
2023-08-08 22:42:34 -0400

    Adding new proj

---
+++ July28/src/Firstone.java
@@ -0,0 +1,20 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Firstone {
+
+	public static void main(String[] args) {
+		//Invoking browser
+		//ChromeDriver driver = new ChromeDriver();
+		
+		//System.setProperty("webdriver.chrome.driver", "/Users/madha/Documents/chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("www.google.com");
+
+	}
+
+}
